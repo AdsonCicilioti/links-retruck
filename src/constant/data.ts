97645ed616d2d3cb579ae277ca76a71dadd05a98
Adson Cicilioti <eu@adsonagencia.com>
2021-08-26 00:27:06 -0300

    first commit

---
+++ src/constant/data.ts
@@ -0,0 +1,57 @@
+interface IDataLink {
+  title: string
+  url: string
+  icon: string
+}
+
+const dataLinks: IDataLink[] = [
+  {
+    title: "WhatsApp Comercial",
+    url: "https://wa.me/5528999583179?text=Ol%C3%A1%21%20Venho%20do%20Link%20na%20Bio%20do%20Instagram%20e%20gostaria%20de%20informa%C3%A7%C3%B5es..",
+    icon: "/icons/whatsapp.png",
+  },
+  {
+    title: "Telefone Fixo",
+    url: "tel:02835371397",
+    icon: "/icons/phone.png",
+  },
+
+  {
+    title: "E-mail Comercial",
+    url: "mailto:comercial@re-truck.com",
+    icon: "/icons/envelope.png",
+  },
+  {
+    title: "4° Eixo Direcional",
+    url: "https://4eixo.com/?ref=linksretruck",
+    icon: "/icons/globe.png",
+  },
+
+  {
+    title: "Localização",
+    url: "https://g.page/ReTruck?share",
+    icon: "/icons/map.png",
+  },
+  {
+    title: "Instagram",
+    url: "https://instagram.com/retruck.oficial",
+    icon: "/icons/instagram.png",
+  },
+  {
+    title: "Facebook",
+    url: "https://fb.com/retruck",
+    icon: "/icons/facebook.png",
+  },
+  {
+    title: "LinkedIn",
+    url: "https://linkedin.com/company/retruck",
+    icon: "/icons/linkedin.png",
+  },
+  {
+    title: "Trabalhe Conosco",
+    url: "https://4eixo.com/vagas",
+    icon: "/icons/briefcase.png",
+  },
+]
+
+export default dataLinks
---
+++ src/pages/index.tsx
@@ -0,0 +1,76 @@
+import type { NextPage } from "next"
+import Head from "next/head"
+import Image from "next/image"
+import dataLinks from "../constant/data"
+import styles from "../styles/Home.module.css"
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Links RETRUCK</title>
+        <meta name="description" content="Confira nossos links." />
+        <link rel="icon" href="/favicon.png" />
+      </Head>
+      <header className={styles.header}>
+        <h1 className={styles.title}>
+          <Image
+            src="/logo-retruck-h.png"
+            alt="Logo RETRUCK"
+            width={256}
+            height={84}
+          />
+          <span className={styles["sr-only"]}>RETRUCK</span>
+        </h1>
+        <p className={styles.description}>
+          Confira abaixo nossos principais links.
+        </p>
+      </header>
+
+      <main className={styles.main}>
+        <div className={styles.grid}>
+          {dataLinks &&
+            dataLinks.map((link, i, links) => (
+              <a
+                key={i}
+                href={link.url}
+                className={styles.card}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <span>
+                  <Image
+                    src={link.icon}
+                    width={32}
+                    height={32}
+                    alt={`${link.title}`}
+                  />
+                </span>
+                <h2>{link.title}</h2>
+              </a>
+            ))}
+        </div>
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://adsonagencia.com/?ref=linksretruck"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Feito por{" "}
+          <span className={styles.logo}>
+            <Image
+              src="/logo_adsonagencia_site.png"
+              alt="adsonagencia.com"
+              width={147}
+              height={18}
+            />
+          </span>
+        </a>
+      </footer>
+    </>
+  )
+}
+
+export default Home
